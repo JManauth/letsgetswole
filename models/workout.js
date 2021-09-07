5158b3c836409881354d0bfc11f8322e2591e7a1
jafet <jafet.manauth@hotmail.com>
2021-09-06 23:31:14 -0500

    trying to seed database

---
+++ models/workout.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+
+    day:{
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type:{
+            type: String,
+            trim: true,
+            required: "Please describe wether this is a cardio or resistance exercise",
+        },
+        name:{
+            type: String,
+            trim: true,
+            required:"Please enter a name for this workout"
+        },
+        duration:{
+            type: Number, 
+        },
+        distance:{
+            type: Number, 
+        },
+        weight:{
+            type: Number, 
+        },
+        reps:{
+            type: Number, 
+        },
+        sets:{
+            type: Number, 
+        }
+    }]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
